911982e1975eb1d58982140f8814c734a9918769
Asa Paparo <asapaparo@gmail.com>
2025-02-01 22:53:03 -0500

    moved builders

--- modules/builders.nix
+++ modules/builders.nix
@@ -6,13 +6,5 @@ with lib; {
     default = {};
   };
 
-  mkNixPakPackage = args: let
-    mkNixPak = inputs.nixpak.lib.nixpak {
-      inherit (nixpkgs) lib;
-      inherit pkgs;
-    };
-    pkg = mkNixPak args;
-  in pkg.config.env;
-
   config._module.args = { inherit (config) builders; };
 }
